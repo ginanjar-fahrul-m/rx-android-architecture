0e305fd9be6ccc7709154529951e88c4fe8e635f
Timo Tuominen <timo.tuominen@gmail.com>
2014-08-02 11:41:42 +0100

    Initial content provider

--- app/src/main/java/com/tehmou/rxbookapp/provider/TwitterContentProvider.java
+++ app/src/main/java/com/tehmou/rxbookapp/provider/TwitterContentProvider.java
@@ -4,6 +4,13 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
+
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.stmt.GenericRowMapper;
+import com.tehmou.rxbookapp.pojo.TwitterStatus;
 
 /**
  * Created by ttuo on 24/06/14.
@@ -11,13 +18,30 @@ import android.net.Uri;
 public class TwitterContentProvider extends ContentProvider {
     static private String TAG = TwitterContentProvider.class.getCanonicalName();
 
+    private DBHelper helper;
+
     @Override
     public boolean onCreate() {
+        helper = OpenHelperManager.getHelper(getContext(), DBHelper.class);
         return false;
     }
 
     @Override
+    public void shutdown() {
+        super.shutdown();
+        OpenHelperManager.releaseHelper();
+    }
+
+    @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        try {
+            Dao<TwitterStatus, String> twitterStatusDao = DaoManager.createDao(helper.getConnectionSource(), TwitterStatus.class);
+            GenericRowMapper<TwitterStatus> mapper = twitterStatusDao.getSelectStarRowMapper();
+            twitterStatusDao.qu
+            return helper.getReadableDatabase().query();
+        } catch (Exception e) {
+            Log.e(TAG, "query(" + uri.toString() + ")", e);
+        }
         return null;
     }
 
